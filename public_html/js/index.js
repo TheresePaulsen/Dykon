ed83b4044ed840812b743f97607f74ced54d72f9
TheresePaulsen <therese-paulsen@live.dk>
2025-11-22 16:30:01 +0200

    remove public_html

--- public_html/js/index.js
+++
@@ -1,9 +0,0 @@
-const bars = document.querySelector("#bars");
-
-const openNav = () => {
-    document.querySelector("header > nav").classList.toggle("show");
-    document.querySelector("#bars i").classList.toggle("fa-bars");
-    document.querySelector("#bars i").classList.toggle("fa-xmark");
-}
-
-bars.addEventListener("click", openNav);
